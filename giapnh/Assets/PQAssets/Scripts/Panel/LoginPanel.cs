ac2769d24f3004cb07a804b5000f3f261fbb7a68
giapnh <hgbk.it@gmail.com>
2014-02-14 13:27:05 -0800

    Signed-off-by: giapnh <hgbk.it@gmail.com>

--- giapnh/Assets/PQAssets/Scripts/Panel/LoginPanel.cs
+++ giapnh/Assets/PQAssets/Scripts/Panel/LoginPanel.cs
@@ -65,7 +65,6 @@ public class LoginPanel : MonoBehaviour {
 		// Check valid username and password
 		if(!InputFilter.CheckEmail(username)){
 			Debug.Log("Invalid username");
-			//
 			controller.SendMessage("HideLoading");
 			//Show notify
 			return;
