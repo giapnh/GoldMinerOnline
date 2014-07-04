ff31a6292586dfebb0fd71ec048514ed489c5851
Nguyen Linh <linhnv@OOTs-MacBook-Air.local>
2014-07-04 15:01:47 +0700

    ip bug

--- giapnh/Assets/PQAssets/Scripts/Net/Configs.cs
+++ giapnh/Assets/PQAssets/Scripts/Net/Configs.cs
@@ -2,7 +2,7 @@ using System;
 public class Configs
 {
 	//public static string HOST = "182.48.50.239";
-	public static string HOST = "192.1.100.40";
+	public static string HOST = "192.168.100.40";
 //	public static string HOST = "localhost";
 	public static int PORT = 5555;//port
 
