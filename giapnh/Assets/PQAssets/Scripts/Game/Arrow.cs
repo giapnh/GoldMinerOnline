460cb620d4e3f99d699be2065bc22b27c1402d6d
linhnv <nguyenvanlinh.bk@gmail.com>
2014-07-17 15:11:10 +0700

    only move after idle

--- giapnh/Assets/PQAssets/Scripts/Game/Arrow.cs
+++ giapnh/Assets/PQAssets/Scripts/Game/Arrow.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Arrow : MonoBehaviour {
 	public GameObject onlineGameScreen;
+	public GameObject character;
 	void Start () {
 	
 	}
@@ -13,15 +14,17 @@ public class Arrow : MonoBehaviour {
 	}
 	
 	void OnClick(){
-		GameObject character =  GameObject.Find("Character");
+//		GameObject character =  GameObject.Find("Character");
 		Hook hook_info = character.gameObject.GetComponentInChildren<Hook>();
+		Character character_info = character.gameObject.GetComponent<Character> ();
 		//check current user' turn
 		OnlineGamePanel onlineGame_info = onlineGameScreen.gameObject.GetComponent<OnlineGamePanel> ();
 		string current_user = onlineGame_info.current_player;
 		float round_time = onlineGame_info.round_time;
-        if(hook_info.state == Hook.IDLE && current_user == PlayerInfo.Username && round_time <=15){
-			int to_pos = int.Parse(this.gameObject.name.Substring(this.gameObject.name.Length - 1));
-			onlineGameScreen.SendMessage("Move", to_pos);
+
+		if (character_info.state != Character.MOVING && hook_info.state == Hook.IDLE && current_user == PlayerInfo.Username && round_time <= 15) {
+				int to_pos = int.Parse (this.gameObject.name.Substring (this.gameObject.name.Length - 1));
+				onlineGameScreen.SendMessage ("Move", to_pos);
 		}
 	}
 }
